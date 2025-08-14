09d80ccc9aa579456e7a94c1c973d393e9c42ea0
aishahnugeo <aishah@nugeosolutions.com>
2025-08-14 08:58:28 +0800

    Insitute migration

---
+++ database/migrations/2025_07_29_060800_create_category_institute_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('category_institute', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->integer('deleted_by');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_institute');
+    }
+};
