a08529f8ddbeaf9c9a965f80dc4032b7945b30b8
aishahnugeo <aishah@nugeosolutions.com>
2025-08-17 09:41:38 +0800

    Push

--- database/migrations/2025_08_15_143400_alter_users_table.php
+++ database/migrations/2025_08_15_143400_alter_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('profile_id')->nullable();
+            $table->unsignedBigInteger('profile_id')->nullable()->change();
         });
     }
 
