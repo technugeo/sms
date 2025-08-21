c464d0fb6771b1279b21c713152a6ff3e3053a71
bosspenyu <ahmad.muhaimi.mohamad@gmail.com>
2025-08-21 16:26:49 +0800

    sheids/spatie/acl

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,15 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-use Spatie\Permission\Traits\HasRoles; 
-
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
-    
+    use HasFactory, Notifiable;
+
     protected $guard_name = 'web';
 
     /**
@@ -29,9 +27,9 @@ class User extends Authenticatable
         'password',
         'profile_type',
         'profile_id',
-        'email_verified_at',   
-        'is_active',          
-        'status',              
+        'email_verified_at',
+        'is_active',
+        'status',
     ];
 
     /**
@@ -54,7 +52,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            
+
         ];
     }
 
